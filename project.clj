dbf73c40597bdf23db924521bcd47b4e44fe9373
jerger <michael.jerger@meissa-gmbh.de>
2022-02-25 10:25:50 +0100

    Version 1.0.2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.domaindrivenarchitecture/c4k-shynet "1.0.2-SNAPSHOT"
+(defproject org.domaindrivenarchitecture/c4k-shynet "1.0.2"
   :description "shynet c4k-installation package"
   :url "https://domaindrivenarchitecture.org"
   :license {:name "Apache License, Version 2.0"
