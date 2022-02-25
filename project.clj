397e8708690767ab46058bfb402b67f456853404
bom <mattis.boeckle@meissa-gmbh.de>
2022-02-25 11:32:59 +0100

    Version 1.0.4

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.domaindrivenarchitecture/c4k-shynet "1.0.4-SNAPSHOT"
+(defproject org.domaindrivenarchitecture/c4k-shynet "1.0.4"
   :description "shynet c4k-installation package"
   :url "https://domaindrivenarchitecture.org"
   :license {:name "Apache License, Version 2.0"
