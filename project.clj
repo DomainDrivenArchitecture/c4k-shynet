238a0abbd08cb44e1a7d882dc528c196149aad55
see <erik.seiert@meissa-gmbh.de>
2022-05-13 15:58:33 +0200

    Version 1.0.6

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.domaindrivenarchitecture/c4k-shynet "1.0.6-SNAPSHOT"
+(defproject org.domaindrivenarchitecture/c4k-shynet "1.0.6"
   :description "shynet c4k-installation package"
   :url "https://domaindrivenarchitecture.org"
   :license {:name "Apache License, Version 2.0"
