2547fa67b6c4b52f4f79c94ee935a1d24084773a
patdyn <erik.seiert@meissa-gmbh.de>
2024-08-06 11:16:07 +0200

    Add and update deps

--- project.clj
+++ project.clj
@@ -3,9 +3,9 @@
   :url "https://domaindrivenarchitecture.org"
   :license {:name "Apache License, Version 2.0"
             :url "https://www.apache.org/licenses/LICENSE-2.0.html"}
-  :dependencies [[org.clojure/clojure "1.11.3"]
+  :dependencies [[org.clojure/clojure "1.11.4"]
                  [org.clojure/tools.reader "1.4.2"]
-                 [org.domaindrivenarchitecture/c4k-common-clj "7.0.0"]
+                 [org.domaindrivenarchitecture/c4k-common-clj "8.0.0"]
                  [hickory "0.7.1" :exclusions [viebel/codox-klipse-theme]]]
   :target-path "target/%s/"
   :source-paths ["src/main/cljc"
@@ -26,7 +26,8 @@
                        :dependencies [[org.clojure/tools.cli "1.1.230"]
                                       [ch.qos.logback/logback-classic "1.5.6"
                                        :exclusions [com.sun.mail/javax.mail]]
-                                      [org.slf4j/jcl-over-slf4j "2.0.13"]]}}
+                                      [org.slf4j/jcl-over-slf4j "2.0.13"]
+                                      [com.github.clj-easy/graal-build-time "1.0.5"]]}}
   :release-tasks [["test"]
                   ["vcs" "assert-committed"]
                   ["change" "version" "leiningen.release/bump-version" "release"]
